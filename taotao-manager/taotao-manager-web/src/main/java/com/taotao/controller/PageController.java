f5ebad8ceabb5caa020e503a578b3c5bf2ec12bc
heropan <574237980@qq.com>
2018-10-04 22:42:02 +0800

    访问主页showIndex方法编写

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Panxiong
+ * @date 2018/10/4 19:16
+ * @projectname taotaoparent
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String showIndex() {
+        return "index";
+    }
+}
